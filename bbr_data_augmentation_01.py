621e572f07eba9fe1cc2b9b813559ad5af8c44f4
basilhan <kingkwang_han@yahoo.com>
2017-11-01 10:03:32 +0800

    Add files via upload

---
+++ bbr_data_augmentation_01.py
@@ -0,0 +1,12 @@
+# Data Augmentation ----------------------------------------
+## Train
+train_width_shift_range = 0.1
+train_height_shift_range = 0.1
+train_rotation_range = 20.
+train_shear_range = 0.1
+train_zoom_range = 0.1
+## Validate
+validate_width_shift_range = 0.1
+validate_height_shift_range = 0.1
+validate_rotation_range = 20.
+#-------------------------------
